92aa12dce38cbd189be81b7bfae314f954caa6e0
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-13 17:50:59 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./Pages/HomePage";
 import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+});
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.App}>
       <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />

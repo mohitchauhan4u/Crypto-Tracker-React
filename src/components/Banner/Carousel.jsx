cd4c3ecd45e17cf719fd0806d37d36d2f9afe49a
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-14 15:18:10 +0530

    creating carousel

--- src/components/Banner/Carousel.jsx
+++ src/components/Banner/Carousel.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core";
+import { TrendingCoins } from "../../config/api";
+
+const useStyles = makeStyles((theme) => ({
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const classes = useStyles();
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins());
+  };
+  return <div className={classes.carousel}>Carousel</div>;
 };
 
 export default Carousel;

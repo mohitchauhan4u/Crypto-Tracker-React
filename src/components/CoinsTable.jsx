6eae80077fbb67d0e43e684d77ddde5029b1ef78
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-14 15:41:37 +0530

    creating coin table

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { CoinList } from "../config/api";
+import { CryptoState } from "../CrypoContext";
 
 const CoinsTable = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { currency } = CryptoState;
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    setCoins(data);
+    setLoading(false);
+  };
+
   return <div>CoinsTable</div>;
 };
 

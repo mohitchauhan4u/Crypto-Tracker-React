6cd24afc8eb689377351b340e6c370a2fe814139
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-14 22:11:39 +0530

    created table head

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -17,7 +17,28 @@ import {
   TableContainer,
   Table,
   Paper,
+  makeStyles,
 } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  row: {
+    backgroundColor: "#16171a",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#131111",
+    },
+    fontFamily: "Montserrat",
+  },
+  pagination: {
+    "& .MuiPaginationItem-root": {
+      color: "gold",
+    },
+  },
+});
+
+export function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
@@ -25,6 +46,7 @@ const CoinsTable = () => {
   const [search, setSearch] = useState();
 
   const { currency } = CryptoState();
+  const classes = useStyles();
 
   const fetchCoins = async () => {
     setLoading(true);
@@ -46,6 +68,14 @@ const CoinsTable = () => {
     },
   });
 
+  const handleSearch = () => {
+    return coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(search) ||
+        coin.symbol.toLowerCase().includes(search)
+    );
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Container style={{ textAlign: "center" }}>
@@ -66,8 +96,25 @@ const CoinsTable = () => {
           {loading ? (
             <LinearProgress style={{ backgroundColor: "gold" }} />
           ) : (
-            <Table>
-              <TableHead></TableHead>
+            <Table aria-label="simple table">
+              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                <TableRow>
+                  {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Montserrat",
+                      }}
+                      key={head}
+                      align={head === "Coin" ? "" : "right"}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody></TableBody>
             </Table>
           )}
         </TableContainer>

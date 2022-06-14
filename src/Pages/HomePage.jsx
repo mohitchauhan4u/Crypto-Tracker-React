91b3cf1e19d24fb93b1ea239fb8c17a96c23a20b
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-14 15:24:39 +0530

    creating copins table

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
+import CoinTable from "../components/CoinsTable";
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <CoinTable />
     </>
   );
 };

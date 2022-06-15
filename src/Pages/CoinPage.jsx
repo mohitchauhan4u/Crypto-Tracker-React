560a7e9bf2bf35c97d11444f2c0990a1fc8fb048
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-15 16:38:28 +0530

    creating coin page

--- src/Pages/CoinPage.jsx
+++ src/Pages/CoinPage.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "../CrypoContext";
+import { SingleCoin } from "../config/api";
+import axios from "axios";
 
 const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
   return <h1>CoinPage</h1>;
 };
 

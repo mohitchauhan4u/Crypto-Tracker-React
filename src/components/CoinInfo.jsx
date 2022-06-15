c8d59f8bfe1297f8c5199b2a9df3d9e2fe9e534a
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-15 21:46:19 +0530

    creating chart component

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HistoricalChart } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
 
-const CoinInfo = () => {
+const CoinInfo = ({ coin }) => {
+  const [historicalData, setHistoricalData] = useState();
+  const [days, setDays] = useState();
+  const [flag, setflag] = useState(false);
+
+  const { currency } = CryptoState();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setflag(true);
+    setHistoricalData(data.prices);
+  };
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchHistoricData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [days]);
+
   return <div>CoinInfo</div>;
 };
 
